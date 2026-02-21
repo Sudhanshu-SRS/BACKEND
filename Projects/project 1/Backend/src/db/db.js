8513f292638a12c1ee1b51cf2b27479067d18793
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-02-21 17:01:47 +0530

    fixe:Leaked Of Env

--- Projects/project 1/Backend/src/db/db.js
+++ Projects/project 1/Backend/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function ConnectDB() {
   await mongoose.connect(
-    "mongodb+srv://sudhanshusakhare808_db_user:Sudhanshu2002@emi-cluster.wmppf5y.mongodb.net/Project-B",
+    process.env.Mongo_URI
   );
   console.log("Connected To DB");
 }
